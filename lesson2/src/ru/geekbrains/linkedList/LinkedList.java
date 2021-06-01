3cdc6e027849e87850fd5d932fd4a9d23d448e40
nikita.baliassniy <nikita.baliassniy@yandex.ru>
2021-06-01 13:48:26 +0300

    lesson2 upd - Link inlined

--- lesson2/src/ru/geekbrains/linkedList/LinkedList.java
+++ lesson2/src/ru/geekbrains/linkedList/LinkedList.java
@@ -18,4 +18,38 @@ public interface LinkedList<T> {
 
     T find(T value);
 
+    class Link<T> {
+        private T data;
+        private Link<T> next;
+        private Link<T> previous;
+
+        public Link(T data) {
+            this.data = data;
+        }
+
+        public Link<T> getNext() {
+            return next;
+        }
+
+        public Link<T> getPrevious() {
+            return previous;
+        }
+
+        public T getValue() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public void setNext(Link<T> next) {
+            this.next = next;
+        }
+
+        public void setPrevious(Link<T> previous) {
+            this.previous = previous;
+        }
+    }
+
 }
